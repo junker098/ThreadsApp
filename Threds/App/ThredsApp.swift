714954f2f6d8bc03df1eae26ec726c0980f7edc2
Yuriy <>
2023-12-14 22:35:20 +0200

    Files

---
+++ Threds/App/ThredsApp.swift
@@ -0,0 +1,18 @@
+//
+//  ThredsApp.swift
+//  Threds
+//
+//  Created by Yuriy on 11.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct ThredsApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Threds/Core/Root/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Threds
+//
+//  Created by Yuriy on 11.12.2023.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+    
+    
+    var body: some View {
+        Text("Hello world")
+        Image(systemName: "mic")
+    }
+    
+}
+
+#Preview {
+    ContentView()
+}
